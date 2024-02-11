d7b9b02c46fa151f207d1efa0540aaf736ba5890
Allen Choi <37539914+Thunderbird2086@users.noreply.github.com>
2024-02-11 19:04:30 +0900

    rename rp2040 firmware from left to nohanded

--- keyboards/unicorne/keymaps/norgb/rules.mk
+++ keyboards/unicorne/keymaps/norgb/rules.mk
@@ -1,2 +1,9 @@
-RGB_MATRIIX_ENABLE = no
-APPLE_FN_ENABLE = yes
+OLED_ENABLE = yes
+RGB_MATRIX_ENABLE = no
+#APPLE_FN_ENABLE = yes
+
+SRC += ./lib/keylogger.c
+SRC += ./lib/layer_state_reader.c
+SRC += ./lib/logo_reader.c
+SRC += ./lib/mode_icon_reader.c
+SRC += oled.c
--- keyboards/unicorne/keymaps/thunderbird2086/rules.mk
+++ keyboards/unicorne/keymaps/thunderbird2086/rules.mk
@@ -7,12 +7,10 @@ TAP_DANCE_ENABLE = yes
 
 APPLE_FN_ENABLE = yes
 
-ifeq ($(strip $(OLED_ENABLE)), yes)
-	SRC += ./lib/layer_state_reader.c
-	SRC += ./lib/logo_reader.c
-	SRC += ./lib/mode_icon_reader.c
-	SRC += oled.c
-endif
+SRC += ./lib/layer_state_reader.c
+SRC += ./lib/logo_reader.c
+SRC += ./lib/mode_icon_reader.c
+SRC += oled.c
 
 SRC += process_records.c
 SRC += rgb.c
