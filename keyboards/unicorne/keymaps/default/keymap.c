60f004671cef266f2dc1877fdef747af7d583814
Allen Choi <37539914+Thunderbird2086@users.noreply.github.com>
2024-03-06 23:09:24 +0900

    fixed an issue with keylogger

--- keyboards/unicorne/keymaps/default/keymap.c
+++ keyboards/unicorne/keymaps/default/keymap.c
@@ -26,6 +26,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Z,      KC_X,      KC_C,      KC_D,      KC_V,                                 KC_K,      KC_H,      KC_COMM,   KC_DOT,    KC_SLSH,
                                          NAV_ESC,   FCT_TAB,   COD_SPC,        COD_ENT,   FCT_BSPC,  MED_DEL
     ),
+#if defined(QWERTY_ENABLED)
+     /*
+      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
+      * │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │
+      * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
+      *           ┌───┐                   ┌───┐
+      *           │ESC├───┐           ┌───┤Del│
+      *           └───┤TAB├───┐   ┌───┤Bsp├───┘
+      *               └───┤Spc│   │Ent├───┘
+      *                   └───┘   └───┘
+      */
+    [_QWERTY] = LAYOUT_split_3x5_3(
+        KC_Q,      KC_W,      KC_F,      KC_P,      KC_B,                                 KC_J,      KC_L,      KC_U,      KC_Y,      KC_QUOT,
+        LOPT_A,    LSFT_S,    LCTL_D,    M_LCMD_F,  KC_G,                                 KC_H,      RCMD_J,    RCTL_K,    RSFT_L,    ROPT_SCLN,
+        KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,                                 KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,
+                                         NAV_ESC,   FCT_TAB,   COD_SPC,        COD_ENT,   FCT_BSPC,  MED_DEL
+    ),
+#endif
      /*
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
       * │ 1 │ 2 │ 3 │ 4 │ 5 │       │ 6 │ 7 │ 8 │ 9 │ 0 │
