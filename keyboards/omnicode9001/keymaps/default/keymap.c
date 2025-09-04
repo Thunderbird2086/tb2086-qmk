e03d6852d20a9a7726ad241eeaeb7a679d40c53c
Allen Choi <37539914+Thunderbird2086@users.noreply.github.com>
2025-09-04 15:52:31 +0900

    Home Row MOD for Colemak-DH

--- keyboards/omnicode9001/keymaps/default/keymap.c
+++ keyboards/omnicode9001/keymaps/default/keymap.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include QMK_KEYBOARD_H
+#include "keycodes.h"
 
 enum custom_keycodes {
     QWERTY = QK_KB_0,   // QWERTY
@@ -82,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC ,          KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,     KC_PSCR, KC_BRMD, KC_BRMU,     _______, KC_MUTE, KC_VOLD, KC_VOLU,
 		KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC,     KC_INS , KC_HOME, KC_PGUP,     KC_NUM , KC_PSLS, KC_PAST, KC_PMNS,
 		KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,     KC_DEL , KC_END , KC_PGDN,     KC_P7  , KC_P8  , KC_P9  , KC_PPLS,
-		KC_CAPS, KC_A   , KC_R   , KC_S   , KC_T   , KC_G   , KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , KC_QUOT,          KC_ENT ,                                    KC_P4  , KC_P5  , KC_P6  ,
+		KC_CAPS, LOPT_A , LSHF_R , LCTL_S , LCMD_T , KC_G   , KC_M   , RCMD_N , RCTL_E , RSHF_I , ROPT_O , KC_QUOT,          KC_ENT ,                                    KC_P4  , KC_P5  , KC_P6  ,
 		KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH,                   KC_RSFT,              KC_UP  ,              KC_P1  , KC_P2  , KC_P3  , KC_PENT,
 		KC_LCTL, KC_LOPT, KC_LCMD,                   KC_SPC ,                            KC_RCMD, KC_ROPT, MO(2)  ,          KC_RCTL,     KC_LEFT, KC_DOWN, KC_RGHT,     KC_P0  ,          KC_PDOT
 	),
